c4652d3811d7cbb21a6a3c1f3f13b83bf5e04892
Dane <dkoval3@gatech.edu>
2021-12-26 23:21:42 -0800

    Initial Commit

---
+++ du-rush-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  du-rush-app
+//
+//  Created by Dane Koval on 12/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ du-rush-app/du_rush_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  du_rush_appApp.swift
+//  du-rush-app
+//
+//  Created by Dane Koval on 12/26/21.
+//
+
+import SwiftUI
+
+@main
+struct du_rush_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
