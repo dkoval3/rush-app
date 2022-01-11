24b22e0f1321cbf4161c0517892722ad2eb43aa4
Dane <dkoval3@gatech.edu>
2022-01-10 21:45:04 -0500

    Added dynamic API calls for GET and POST

--- du-rush-app/api/APICall.swift
+++ du-rush-app/api/APICall.swift
@@ -7,10 +7,19 @@
 
 import Foundation
 
+let BASE_ENDPOINT = "http://localhost:3000"
+
 class RushDatabaseCalls {
+    
+    enum CustomError: Error {
+        case invalidURL
+        case invalidCredentials
+        case invalidDatat
+    }
+    
     // Use this function to create the request object
     func createReqeust(httpMethod: String, path: String, body: [String: AnyHashable] = [:]) -> URLRequest? {
-        guard let url = URL(string: "http://localhost:3000\(path)") else { return nil }
+        guard let url = URL(string: "\(BASE_ENDPOINT)\(path)") else { return nil }
         var request = URLRequest(url: url)
         request.httpMethod = httpMethod
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
@@ -37,19 +46,55 @@ class RushDatabaseCalls {
         task.resume()
     }
     
-    func getUserCredentials(body: [String:AnyHashable], completionBlock: @escaping ([UserCreds]) -> Void) {
-        guard let request = self.createReqeust(httpMethod: "POST", path: "/auth/login", body: body) else { return }
-        let task = URLSession.shared.dataTask(with: request) { data, _, err in
-            guard let data = data, err == nil else {
-                print(err ?? "Error: data is nil")
+    func getRequest<T: Codable>(expecting: T.Type, path: String, completion: @escaping (Result<T, Error>) -> Void) {
+        
+        guard let url = URL(string: "\(BASE_ENDPOINT)\(path)") else {
+            completion(.failure(CustomError.invalidURL))
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let task = URLSession.shared.dataTask(with: request) { data, _, error in
+            guard let data = data, error == nil else {
+                completion(.failure(CustomError.invalidData))
                 return
             }
             
             do {
-                let response = try JSONDecoder().decode([UserCreds].self, from: data)
-                completionBlock(response)
+                let response = try JSONDecoder().decode(expecting, from: data)
+                completion(.success(response))
             } catch {
-                print(error)
+                completion(.failure(error))
+            }
+        }
+        task.resume()
+    }
+    
+    func postRequest<T: Codable>(expecting: T.Type, path: String, body: [String:AnyHashable], completion: @escaping (Result<T, Error>) -> Void) {
+        guard let url = URL(string: "\(BASE_ENDPOINT)\(path)") else {
+            completion(.failure(CustomError.invalidURL))
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = try? JSONSerialization.data(withJSONObject: body, options: .fragmentsAllowed)
+        
+        let task = URLSession.shared.dataTask(with: request) { data, _, error in
+            guard let data = data, error == nil else {
+                completion(.failure(CustomError.invalidData))
+                return
+            }
+            
+            do {
+                let response = try JSONDecoder().decode(expecting, from: data)
+                completion(.success(response))
+            } catch {
+                completion(.failure(error))
             }
         }
         task.resume()
